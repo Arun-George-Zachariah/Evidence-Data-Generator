6af9f6d7943fe0ec8c9d63fedfaacb41f8344978
Arun George Zachariah <azachariah@mail.missouri.edu>
2020-06-09 21:47:05 -0500

    Changes done for generating evidence.

---
+++ src/main/scala/edu/missouri/Constants/Constants.scala
@@ -0,0 +1,46 @@
+package edu.missouri.Constants
+
+object Constants {
+  // General Constants
+  val APP_NAME = "Evidence-Data-Generator"
+  val TWEETS_VIEW = "Tweets"
+  val USER = "user"
+  val TWEET_ID = "tweetId"
+  val RETWEETED_STATUS_ID = "retweetedStatusId"
+  val RETWEETED_STATUS_USER_ID = "retweetedStatusUserId"
+  val URL = "url"
+  val MENTIONED_LST = "mentionedLst"
+  val HASHTAGS = "hashtags"
+
+  // Predicate Constants
+  val TWEETED = "tweeted"
+  val CONTAINS_LINK = "containsLink"
+  val MENTIONS = "mentions"
+  val RETWEETED = "retweeted"
+  val CONTAINS_HASHTAG = "containsHashtag"
+  val VERIFIED = "verified"
+  val IS_POSSIBLY_SENSITIVE = "isPossiblySensitive"
+  val FRIENDS_COUNT = "friendsCount"
+  val FOLLOWERS_COUNT = "followersCount"
+  val STATUS_COUNT = "statusesCount"
+  val RETWEET_COUNT = "retweetCount"
+
+  // Query Constants
+  var SELECT_QUERY = "SELECT id as " + TWEET_ID + ", "
+    "retweetCount as " + RETWEET_COUNT + ", "
+    "isPossiblySensitive as " + IS_POSSIBLY_SENSITIVE + ", "
+    "userMentionEntities as " + MENTIONED_LST + ", " +
+    "hashtagEntities as  " + HASHTAGS + ", " +
+    "urlEntities.expandedURL as " + URL + ", " +
+    "user.id as " + USER + ", " +
+    "user.isVerified as " + VERIFIED + ", " +
+    "user.followersCount as " + FOLLOWERS_COUNT + ", " +
+    "user.friendsCount as " + FRIENDS_COUNT + ", "
+    "user.statusesCount as " + STATUS_COUNT + ", "
+    "retweetedStatus.id as " + RETWEETED_STATUS_ID + ", "
+    "retweetedStatus.retweetCount as " + RETWEET_COUNT + ", " +
+    "retweetedStatus.user.id as " + RETWEETED_STATUS_USER_ID + ", " +
+    "FROM " + TWEETS_VIEW
+
+
+}
--- src/main/scala/edu/missouri/GenerateEvidence.scala
+++ src/main/scala/edu/missouri/GenerateEvidence.scala
@@ -1,51 +1,122 @@
 package edu.missouri
 
+import java.io.{BufferedReader, BufferedWriter, File, FileReader, FileWriter}
 import org.apache.spark.sql.SparkSession
+import edu.missouri.Constants.Constants
+import org.apache.spark.sql.Row
 
-class GenerateEvidence {
-  def main(args: Array[String]) = {
-    //Validating the input.
-    if (args.length < 4) {
-      println("Usage: GenerateEvidence " + "<input_tweets_directory> " + "<domains_file> <urls_file> <output_file> <collect_rel_bool> {rao,katib}")
-      System.exit(-1)
+object GenerateEvidence {
+  def constructEvidence(inFile: String): Unit = {
+    // Defining the Spark SQL Context.
+    val sqlContext = SparkSession.builder.master("local[*]").appName(Constants.APP_NAME).getOrCreate()
+
+    var reader:BufferedReader = null
+    var writer:BufferedWriter = null
+    try {
+      reader = new BufferedReader(new FileReader(new File(inFile)))
+      writer = new BufferedWriter(new FileWriter(new File("out/evidence.db")))
+
+      // Loading the tweets to a table.
+      val myTweets = sqlContext.read.json(inFile)
+      myTweets.createOrReplaceTempView(Constants.TWEETS_VIEW)
+
+      // Querying the tweets.
+      var results = sqlContext.sql(Constants.SELECT_QUERY)
+
+      results.foreach(x => {
+        // tweeted predicate
+        val user = x.getAs[Long](Constants.USER)
+        val tweetId = x.getAs[Long](Constants.TWEET_ID)
+        writer.write(Constants.TWEETED + "(" + user + "," + tweetId + ")")
+
+        // verified predicate
+        val isVerified = x.getAs[Boolean](Constants.VERIFIED)
+        if (isVerified) {
+          writer.write(Constants.VERIFIED + "(" + user + ")")
+        }
+
+        val retweetedStatusId = x.getAs[Long](Constants.RETWEETED_STATUS_ID)
+        if (retweetedStatusId != null) {
+          // retweeted predicate
+          writer.write(Constants.RETWEETED + "(" + user + "," + retweetedStatusId + ")")
+
+          // tweeted predicate
+          val retweetedStatusUserId = x.getAs[Long](Constants.RETWEETED_STATUS_USER_ID)
+          writer.write(Constants.TWEETED + "(" + retweetedStatusUserId + "," + retweetedStatusId + ")")
+
+          // retweetCount predicate
+          val retweetedStatusRetweetCount = x.getAs[Long](Constants.RETWEET_COUNT)
+          writer.write(Constants.RETWEET_COUNT + "(" + retweetedStatusId + "," + retweetedStatusRetweetCount + ")")
+        }
+
+        // isPossiblySensitive predicate
+        val isPossiblySensitive = x.getAs[Boolean](Constants.IS_POSSIBLY_SENSITIVE)
+        if (isPossiblySensitive) {
+          writer.write(Constants.IS_POSSIBLY_SENSITIVE + "(" + tweetId + ")")
+        }
+
+        // statusesCount predicate
+        val statusCount = x.getAs[Long](Constants.STATUS_COUNT)
+        writer.write(Constants.STATUS_COUNT + "(" + user + "," + statusCount + ")")
+
+        // friendsCount predicate
+        val friendsCount = x.getAs[Int](Constants.FRIENDS_COUNT)
+        writer.write(Constants.FRIENDS_COUNT + "(" + user + "," + friendsCount + ")")
+
+        // followersCount Predicate
+        val followersCount = x.getAs[Int](Constants.FOLLOWERS_COUNT)
+        writer.write(Constants.FOLLOWERS_COUNT + "(" + user + "," + followersCount + ")")
+
+        // containsLink Predicate
+        var links = x.getAs[Seq[String]](Constants.URL)
+        if(links != null && links.length > 0) {
+          for(link <- links) {
+            writer.write(Constants.CONTAINS_LINK + "(" + tweetId + "," + link + ")")
+          }
+        }
+
+        // mentions Predicate
+        var metionedLst = x.getAs[Seq[Row]](Constants.MENTIONED_LST)
+        if(metionedLst != null && metionedLst.length > 0) {
+          for(mentions <- metionedLst) {
+            writer.write(Constants.MENTIONS + "(" + tweetId + "," + mentions.getAs[Long]("id") + ")")
+          }
+        }
+
+        // containsHashtag Predicate
+        var hashtags = x.getAs[Seq[Row]](Constants.HASHTAGS)
+        if(hashtags != null && hashtags.length > 0) {
+          for(hashtag <- hashtags) {
+            writer.write(Constants.CONTAINS_HASHTAG + "(" + tweetId + "," + hashtag.getAs[String]("text") + ")")
+          }
+        }
+
+      })
+
+      // Starting the Streaming process.
+      print("CollectTweets :: getTwitterData :: Collecting Twitter tweets via streaming APIs...")
+    } catch {
+      case e: Exception =>
+        System.out.println("GenerateEvidence :: constructEvidence :: Exception encountered while writing to the file")
+        e.printStackTrace()
+        System.exit(-1)
+    } finally {
+      try {
+        reader.close()
+      } catch {
+        case e:Exception => {
+          System.out.println("GenerateEvidence :: constructEvidence :: Exception encountered while closing the BufferedReader")
+          System.exit(-1)
+        }
+      }
+      try {
+        writer.close()
+      } catch {
+        case e:Exception => {
+          System.out.println("GenerateEvidence :: constructEvidence :: Exception encountered while closing the BufferedWriter")
+          System.exit(-1)
+        }
+      }
     }
-
-    val tweetDataPath = args(0)
-
-    // Creating the Spark SQL context.
-    val sqlContext = SparkSession.builder.master("local").getOrCreate()
-
-    // Loading the tweets file.
-    val myTweets = sqlContext.read.json(tweetDataPath)
-    myTweets.createOrReplaceTempView("Tweets")
-
-    // Forming the Spark SQL query.
-    var query = "SELECT id as t, retweetCount, isPossiblySensitive as sensitive, " +
-      "userMentionEntities as mentioned, " +
-      "hashtagEntities as hashtags, " +
-      "urlEntities.expandedURL as fullLinks, " +
-      "user.id as user, " +
-      "user.isVerified as isVerified, " +
-      "user.followersCount as followersCount, " +
-      "user.friendsCount as friendsCount, " +
-      "user.statusesCount as statusesCount, " +
-      "retweetedStatus.id as rsT, " +
-      "retweetedStatus.retweetCount as rsRetweetCount, " +
-      "retweetedStatus.user.id as rsUser " +
-      "FROM Tweets"
-
-    var results = sqlContext.sql(query)
-    //results.registerTempTable("Tweets")
-
-    results.foreach(x => {
-      val usr = x.getAs[Long]("user")
-      val tid = x.getAs[Long]("t")
-      val verified = x.getAs[Boolean]("isVerified")
-      val retweetedStatusTid = x.getAs[Long]("rsT")
-
-      println("Output :: usr :: " + usr + " :: tid :: " + tid)
-
-    })
-
   }
 }
