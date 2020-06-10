1e97d5fe41ba27a6670e45fd7ef2fab7c30d0a78
Arun George Zachariah <azachariah@mail.missouri.edu>
2020-06-09 22:14:38 -0500

    Minor changes done

--- src/main/scala/edu/missouri/GenerateEvidence.scala
+++ src/main/scala/edu/missouri/GenerateEvidence.scala
@@ -1,14 +1,14 @@
 package edu.missouri
 
 import java.io.{BufferedReader, BufferedWriter, File, FileReader, FileWriter}
+
 import org.apache.spark.sql.SparkSession
-import edu.missouri.Constants.Constants
 import org.apache.spark.sql.Row
 
 object GenerateEvidence {
   def constructEvidence(inFile: String): Unit = {
     // Defining the Spark SQL Context.
-    val sqlContext = SparkSession.builder.master("local[*]").appName(Constants.APP_NAME).getOrCreate()
+    val sqlContext = SparkSession.builder.master("local[*]").appName(Constants.Constants.APP_NAME).getOrCreate()
 
     var reader:BufferedReader = null
     var writer:BufferedWriter = null
@@ -18,76 +18,76 @@ object GenerateEvidence {
 
       // Loading the tweets to a table.
       val myTweets = sqlContext.read.json(inFile)
-      myTweets.createOrReplaceTempView(Constants.TWEETS_VIEW)
+      myTweets.createOrReplaceTempView(Constants.Constants.TWEETS_VIEW)
 
       // Querying the tweets.
-      var results = sqlContext.sql(Constants.SELECT_QUERY)
+      var results = sqlContext.sql(Constants.Constants.SELECT_QUERY)
 
       results.foreach(x => {
         // tweeted predicate
-        val user = x.getAs[Long](Constants.USER)
-        val tweetId = x.getAs[Long](Constants.TWEET_ID)
-        writer.write(Constants.TWEETED + "(" + user + "," + tweetId + ")")
+        val user = x.getAs[Long](Constants.Constants.USER)
+        val tweetId = x.getAs[Long](Constants.Constants.TWEET_ID)
+        writer.write(Constants.Constants.TWEETED + "(" + user + "," + tweetId + ")")
 
         // verified predicate
-        val isVerified = x.getAs[Boolean](Constants.VERIFIED)
+        val isVerified = x.getAs[Boolean](Constants.Constants.VERIFIED)
         if (isVerified) {
-          writer.write(Constants.VERIFIED + "(" + user + ")")
+          writer.write(Constants.Constants.VERIFIED + "(" + user + ")")
         }
 
-        val retweetedStatusId = x.getAs[Long](Constants.RETWEETED_STATUS_ID)
+        val retweetedStatusId = x.getAs[Long](Constants.Constants.RETWEETED_STATUS_ID)
         if (retweetedStatusId != null) {
           // retweeted predicate
-          writer.write(Constants.RETWEETED + "(" + user + "," + retweetedStatusId + ")")
+          writer.write(Constants.Constants.RETWEETED + "(" + user + "," + retweetedStatusId + ")")
 
           // tweeted predicate
-          val retweetedStatusUserId = x.getAs[Long](Constants.RETWEETED_STATUS_USER_ID)
-          writer.write(Constants.TWEETED + "(" + retweetedStatusUserId + "," + retweetedStatusId + ")")
+          val retweetedStatusUserId = x.getAs[Long](Constants.Constants.RETWEETED_STATUS_USER_ID)
+          writer.write(Constants.Constants.TWEETED + "(" + retweetedStatusUserId + "," + retweetedStatusId + ")")
 
           // retweetCount predicate
-          val retweetedStatusRetweetCount = x.getAs[Long](Constants.RETWEET_COUNT)
-          writer.write(Constants.RETWEET_COUNT + "(" + retweetedStatusId + "," + retweetedStatusRetweetCount + ")")
+          val retweetedStatusRetweetCount = x.getAs[Long](Constants.Constants.RETWEET_COUNT)
+          writer.write(Constants.Constants.RETWEET_COUNT + "(" + retweetedStatusId + "," + retweetedStatusRetweetCount + ")")
         }
 
         // isPossiblySensitive predicate
-        val isPossiblySensitive = x.getAs[Boolean](Constants.IS_POSSIBLY_SENSITIVE)
+        val isPossiblySensitive = x.getAs[Boolean](Constants.Constants.IS_POSSIBLY_SENSITIVE)
         if (isPossiblySensitive) {
-          writer.write(Constants.IS_POSSIBLY_SENSITIVE + "(" + tweetId + ")")
+          writer.write(Constants.Constants.IS_POSSIBLY_SENSITIVE + "(" + tweetId + ")")
         }
 
         // statusesCount predicate
-        val statusCount = x.getAs[Long](Constants.STATUS_COUNT)
-        writer.write(Constants.STATUS_COUNT + "(" + user + "," + statusCount + ")")
+        val statusCount = x.getAs[Long](Constants.Constants.STATUS_COUNT)
+        writer.write(Constants.Constants.STATUS_COUNT + "(" + user + "," + statusCount + ")")
 
         // friendsCount predicate
-        val friendsCount = x.getAs[Int](Constants.FRIENDS_COUNT)
-        writer.write(Constants.FRIENDS_COUNT + "(" + user + "," + friendsCount + ")")
+        val friendsCount = x.getAs[Int](Constants.Constants.FRIENDS_COUNT)
+        writer.write(Constants.Constants.FRIENDS_COUNT + "(" + user + "," + friendsCount + ")")
 
         // followersCount Predicate
-        val followersCount = x.getAs[Int](Constants.FOLLOWERS_COUNT)
-        writer.write(Constants.FOLLOWERS_COUNT + "(" + user + "," + followersCount + ")")
+        val followersCount = x.getAs[Int](Constants.Constants.FOLLOWERS_COUNT)
+        writer.write(Constants.Constants.FOLLOWERS_COUNT + "(" + user + "," + followersCount + ")")
 
         // containsLink Predicate
-        var links = x.getAs[Seq[String]](Constants.URL)
+        var links = x.getAs[Seq[String]](Constants.Constants.URL)
         if(links != null && links.length > 0) {
           for(link <- links) {
-            writer.write(Constants.CONTAINS_LINK + "(" + tweetId + "," + link + ")")
+            writer.write(Constants.Constants.CONTAINS_LINK + "(" + tweetId + "," + link + ")")
           }
         }
 
         // mentions Predicate
-        var metionedLst = x.getAs[Seq[Row]](Constants.MENTIONED_LST)
+        var metionedLst = x.getAs[Seq[Row]](Constants.Constants.MENTIONED_LST)
         if(metionedLst != null && metionedLst.length > 0) {
           for(mentions <- metionedLst) {
-            writer.write(Constants.MENTIONS + "(" + tweetId + "," + mentions.getAs[Long]("id") + ")")
+            writer.write(Constants.Constants.MENTIONS + "(" + tweetId + "," + mentions.getAs[Long]("id") + ")")
           }
         }
 
         // containsHashtag Predicate
-        var hashtags = x.getAs[Seq[Row]](Constants.HASHTAGS)
+        var hashtags = x.getAs[Seq[Row]](Constants.Constants.HASHTAGS)
         if(hashtags != null && hashtags.length > 0) {
           for(hashtag <- hashtags) {
-            writer.write(Constants.CONTAINS_HASHTAG + "(" + tweetId + "," + hashtag.getAs[String]("text") + ")")
+            writer.write(Constants.Constants.CONTAINS_HASHTAG + "(" + tweetId + "," + hashtag.getAs[String]("text") + ")")
           }
         }
 
@@ -119,4 +119,15 @@ object GenerateEvidence {
       }
     }
   }
+
+  def main(args: Array[String]) = {
+    // Validating input arguments.
+    if (args.length != 1) {
+      println("Usage: GenerateEvidence " + "<TWEETS_FILE> ")
+      System.exit(-1)
+    } else {
+      constructEvidence(args(0))
+    }
+  }
+
 }
